fc2126d2f8bbb3cf105cf09430dd5f77150a9bf7
justinkannekens <justinkannekens@gmail.com>
2023-03-06 22:34:39 +0100

    Header done

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -14,19 +14,39 @@ class Body extends StatelessWidget {
           child: Stack(
             children: <Widget>[
               Container(
+                padding: const EdgeInsets.only(
+                    left: kDefaultPadding,
+                    right: kDefaultPadding,
+                    bottom: 36 + kDefaultPadding),
                 height: size.height * 0.2 - 27,
                 decoration: const BoxDecoration(
                     color: kPrimaryColor,
                     borderRadius: BorderRadius.only(
                         bottomLeft: Radius.circular(36),
                         bottomRight: Radius.circular(36))),
+                child: Row(children: <Widget>[
+                  Text(
+                    "Hi Justin",
+                    style: Theme.of(context).textTheme.headline5?.copyWith(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                  const Spacer(),
+                  const Icon(
+                    Icons.account_circle,
+                    color: Colors.white,
+                    size: 52,
+                  )
+                ]),
               ),
               Positioned(
                   bottom: 0,
                   left: 0,
                   right: 0,
                   child: Container(
+                    alignment: Alignment.center,
                     margin:
+                        const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+                    padding:
                         const EdgeInsets.symmetric(horizontal: kDefaultPadding),
                     height: 54,
                     decoration: BoxDecoration(
@@ -42,7 +62,13 @@ class Body extends StatelessWidget {
                         decoration: InputDecoration(
                             hintText: "Search Recipes",
                             hintStyle: TextStyle(
-                                color: kPrimaryColor.withOpacity(0.5)))),
+                                color: kPrimaryColor.withOpacity(0.6)),
+                            enabledBorder: InputBorder.none,
+                            focusedBorder: InputBorder.none,
+                            suffixIcon: const Icon(
+                              Icons.search,
+                              color: kPrimaryColor,
+                            ))),
                   )),
             ],
           ),
